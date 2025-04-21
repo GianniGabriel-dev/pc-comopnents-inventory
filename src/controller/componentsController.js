c6736aaf4ff602b770a466e5823a70fc884873f7
Gianni Gabriel <clondireanug@gmail.com>
2025-04-21 23:59:43 +0200

    feat:add try catch handling errors of controllers

--- src/controller/componentsController.js
+++ src/controller/componentsController.js
@@ -1,20 +1,31 @@
 import { createNewComponent, getAllComponents, getComponentById } from "../db/queries.js"
 
 export const getComponentPage = async(req, res)=>{
-    const components = await getAllComponents()
-    console.log(components)
-    res.render("components", { 
-        title:"PC Components",
-        components:components
-    })
+    try{
+        const components = await getAllComponents()
+        console.log(components)
+        res.render("components", { 
+            title:"PC Components",
+            components:components
+        })
+    }catch(error){
+        console.error("Error getting components", error)
+        res.status(500).send("Error getting components")
+    }
 }
 export const getComponentDetailsPage = async(req, res)=>{
-    const { component_id } = req.params//obtener id por la url
-    const component= await getComponentById(component_id)
-    res.render("componentDetails",{
-        title:component.component_name,
-        component:component
-    })
+    try{
+        const { component_id } = req.params//obtener id por la url
+        const component= await getComponentById(component_id)
+        res.render("componentDetails",{
+            title:component.component_name,
+            component:component
+        })
+    }catch(error){
+        console.error("Error getting component details", error)
+        res.status(500).send("Error getting component details")
+    }
+
 }
 
 export const getAddComponentPage =(req, res)=>{
@@ -23,12 +34,18 @@ export const getAddComponentPage =(req, res)=>{
     })
 }
 export const postNewComponent =(req, res)=>{
-    const { component_name, component_type, price} = req.body;
+    try{
+        const { component_name, component_type, price} = req.body;
 
-    const component_image = req.file.path; //ruta del archivo subido a cloudinary
+        const component_image = req.file ? req.file.path : null; // Si no se sube imagen, se asigna null
+    
+        createNewComponent(component_name, component_type, price, component_image)
+        //falta enviar los datos a la base de datos
+        console.log(`New component Created: ${component_name}, type: ${component_type}, brand: , price: ${price}, imageURL: ${component_image}`);
+        res.redirect("/components");
+    } catch(error){
+        console.error("Error creating new component", error)
+        res.status(500).send("Error creating new component")
+    }
 
-    createNewComponent(component_name, component_type, price, component_image)
-    //falta enviar los datos a la base de datos
-    console.log(`New component Created: ${component_name}, type: ${component_type}, brand: , price: ${price}, imageURL: ${component_image}`);
-    res.redirect("/components");
 }
--- src/controller/pcController.js
+++ src/controller/pcController.js
@@ -2,42 +2,67 @@ import { createNewPc, getAllComponents, getAllPcs, getPcById, insertComponentsIn
 
 // pagina pricnipal donde se muestran todos los pcs creados
 export const getHomePage = async(req, res)=>{
-    const pcs = await getAllPcs()
+    try{
+        const pcs = await getAllPcs()
  
-    res.render("home", { 
-        title:"Home Page",
-        pcs:pcs
-    })
+        res.render("home", { 
+            title:"Home Page",
+            pcs:pcs
+        })
+        
+    }catch(error){
+        console.error("Error getting pcs", error)
+        res.status(500).send("Error getting pcs")
+    }
+
 }
 
 // pagina en la que aparece detalles de un pc seleccionado por el usario
 export const getPcPage = async(req, res)=>{
-    const { pc_id } = req.params//obtener id por la url
-    const pc= await getPcById(pc_id)
-    res.render("pcDetails",{
-        title:"PC Details",
-        pc:pc
-    })
+    try{
+        const { pc_id } = req.params//obtener id por la url
+        const pc= await getPcById(pc_id)
+        console.log(pc)
+        res.render("pcDetails",{
+            title:"PC Details",
+            pc:pc
+        })
+    }catch(error){
+        console.error("Error getting pc details", error)
+        res.status(500).send("Error getting pc details")
+
+    }
+
 }
 
 //pagina de creacion de un nuevo pc
 export const getCreatePage = async(req, res)=>{
-    const components= await getAllComponents() //se obtiene todos los componentes de la base de datos para mostralos en los input del formulario
-    res.render("createPc", { 
-        title:"Create PC",
-        components:components
-    })
+    try{
+        const components= await getAllComponents() //se obtiene todos los componentes de la base de datos para mostralos en los input del formulario
+        res.render("createPc", { 
+            title:"Create PC",
+            components:components
+        })
+    }catch(error){
+        console.error("Error getting components", error)
+        res.status(500).send("Error getting components")
+    }
 }
 
 //peticion post para crear un nuevo pc
 export const postNewPc = async(req, res)=>{
-    console.log("Formulario recibido", req.body);
-    const { name, cpu, gpu, ram, storage, motherboard, psu, pcCase, cooler } = req.body; //se recogen del body de la peticion los datos del formulario
+    try{
+        console.log("Formulario recibido", req.body);
+        const { name, cpu, gpu, ram, storage, motherboard, psu, pcCase, cooler } = req.body; //se recogen del body de la peticion los datos del formulario
+    
+        const pcID=  await createNewPc(name) //se crea el nuevo pc en la base de datos y se obtiene su id
+        await insertComponentsInPc(cpu, gpu, ram, storage, motherboard, psu, pcCase, cooler, pcID) //se insertan los componentes del pc creado en la base de datos
+    
+        console.log(`New PC Created: ${name}, CPU: ${cpu}, GPU: ${gpu}, RAM: ${ram}, Storage: ${storage}`);
+        res.redirect("/");
+    }catch(error){
+        console.error("Error creating new pc", error)
+        res.status(500).send("Error creating new pc")
+    }
 
-    const pcID=  await createNewPc(name) //se crea el nuevo pc en la base de datos y se obtiene su id
-    await insertComponentsInPc(cpu, gpu, ram, storage, motherboard, psu, pcCase, cooler, pcID) //se insertan los componentes del pc creado en la base de datos
-
-    //falta enviar los datos a la base de datos
-    console.log(`New PC Created: ${name}, CPU: ${cpu}, GPU: ${gpu}, RAM: ${ram}, Storage: ${storage}`);
-    res.redirect("/");
 }
